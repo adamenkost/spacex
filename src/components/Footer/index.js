c7dae55ef5a566349ab3cdf4aa662a4f4a15f556
Nikita <nikitaadamenko@yandex.ru>
2020-10-13 22:32:52 +0300

    fix footer.js

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -18,8 +18,7 @@ const Footer = () => (
 		<p className="footer-text">
 			For additional questions, contact
 			<a className="footer-link" href="mailto:rideshare@spacex.com"
-			>rideshare@spacex.com</a
-			>
+			>rideshare@spacex.com</a>
 		</p>
 
 	</footer>
